7c2a3fc1185614b816beb2f29d696d7190b2e68a
Eric Peralli <eric.peralli@microsoft.com>
2019-05-08 18:14:04 -0600

    Edited integration test files to use server constant

--- src/app/tests/actor.integration.ts
+++ src/app/tests/actor.integration.ts
@@ -1,43 +1,17 @@
 import * as chai from "chai";
 import chaiHttp = require("chai-http");
 import "mocha";
-<<<<<<< HEAD
 import { integrationServer } from "../../config/constants";
-=======
 import { NumberUtilities } from "../../utilities/numberUtilities";
 
-const server = process.env.integration_server_url;
 const numUtility = new NumberUtilities();
->>>>>>> upstream/master
 
 chai.use(chaiHttp);
 
 describe("Testing Actor Controller Methods", () => {
 
-<<<<<<< HEAD
-  it("Testing /api/actors", async () => {
-    const queryId = "nm0000323";
-    return chai.request(integrationServer)
-    .get(`/api/actors`)
-    .then((res) => {
-      chai.expect(res).to.have.status(200);
-      const id = res.body[0].actorId;
-      chai.assert.equal(queryId, id);
-    });
-  });
-
-  it("Testing /api/actors/:id", async () => {
-    const queryId = "nm0000323";
-    return chai.request(integrationServer)
-    .get(`/api/actors/${queryId}`)
-    .then((res) => {
-      chai.expect(res).to.have.status(200);
-      const id = res.body[0].actorId;
-      chai.assert.equal(queryId, id);
-    });
-=======
   it("Testing GET /api/actors", async () => {
-    return chai.request(server)
+    return chai.request(integrationServer)
       .get(`/api/actors`)
       .then((res) => {
         chai.expect(res).to.have.status(200);
@@ -50,21 +24,20 @@ describe("Testing Actor Controller Methods", () => {
     const randomNumber = numUtility.getRandomNumber();
     const actor = {
       actorId: "${randomNumber}",
-      type: "Actor",
-// tslint:disable-next-line: object-literal-sort-keys
-      name: "someName",
       birthYear: 1997,
+      name: "someName",
       profession: ["actor"],
+      type: "Actor",
     };
 
-    chai.request(server)
+    return chai.request(integrationServer)
       .post("/api/actors")
       .set("content-type", "application/json")
       .send(actor)
       .then((res) => {
         chai.expect(res).to.have.status(201);
-        return chai.request(server)
-          .get(`/api/actors/${randomNumber}`)
+        return chai.request(integrationServer)
+        .get(`/api/actors/${randomNumber}`)
           .then((getResponse) => {
             chai.expect(getResponse).to.have.status(200);
             const body = getResponse.body;
@@ -74,7 +47,6 @@ describe("Testing Actor Controller Methods", () => {
             chai.assert.equal(randomNumber, id);
           });
       });
->>>>>>> upstream/master
   });
 
 });
--- src/app/tests/movie.integration.ts
+++ src/app/tests/movie.integration.ts
@@ -1,27 +1,17 @@
 import * as chai from "chai";
 import chaiHttp = require("chai-http");
 import "mocha";
-<<<<<<< HEAD
 import { integrationServer } from "../../config/constants";
-=======
 import { StringUtilities } from "../../utilities/stringUtilities";
 
-const server = process.env.integration_server_url;
 const stringUtil = new StringUtilities();
->>>>>>> upstream/master
 
 chai.use(chaiHttp);
 
 describe("Testing Movie Controller Methods", () => {
 
-<<<<<<< HEAD
-  it("Testing /api/movies", async () => {
-    const movieId = "tt0347779";
-    return chai.request(integrationServer)
-=======
   it("Testing GET /api/movies", async () => {
-    return chai.request(server)
->>>>>>> upstream/master
+    return chai.request(integrationServer)
     .get(`/api/movies`)
     .then((res) => {
       chai.expect(res).to.have.status(200);
@@ -30,12 +20,6 @@ describe("Testing Movie Controller Methods", () => {
     });
   });
 
-<<<<<<< HEAD
-  it("Testing /api/movies/:id", async () => {
-    const queryId = "tt0120737";
-    return chai.request(integrationServer)
-    .get(`/api/movies/${queryId}`)
-=======
   const randomString = stringUtil.getRandomString();
 
   const testMovie = {
@@ -50,16 +34,15 @@ describe("Testing Movie Controller Methods", () => {
   };
 
   it("Testing POST + GET /api/movies/:id", async () => {
-    return chai.request(server)
+    return chai.request(integrationServer)
     .post("/api/movies")
     .set("content-type", "application/json")
     .send(testMovie)
->>>>>>> upstream/master
     .then((res) => {
 
       chai.expect(res).to.have.status(201);
-      return chai.request(server)
-        .get(`/api/movies/${randomString}`)
+      return chai.request(integrationServer)
+      .get(`/api/movies/${randomString}`)
         .then((getResponse) => {
           chai.expect(getResponse).to.have.status(200);
           const getRespBody = getResponse.body;
